7a40ce05e836c6095dc80c47568f88c23e01ff99
BarraHarrison <143306095+BarraHarrison@users.noreply.github.com>
2025-02-17 18:46:30 +0900

    Going to get a wavy, 3D surface

--- main_3d.py
+++ main_3d.py
@@ -2,4 +2,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 def z_function(x, y):
-    pass
+    return np.sin(5 * x) * np.cos(5 * y) / 5
+
+
+def calculate_gradient(x, y):
+    return np.cos(5 * x) * np.cos(5 * y), -np.sin(5 * x) * np.sin(5 * y)
